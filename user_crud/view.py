e6390356b9e152315070e4f556043bbaf2f54ef0
Michael James Soria <michaeljamessoria@Michaels-MacBook-Pro.local>
2025-07-11 17:57:55 +1200

    add deduction on view cart

--- user_crud/view.py
+++ user_crud/view.py
@@ -68,6 +68,8 @@ def edit_order(user):
     account = entry_finder(user)
     acc = account[0]
     acc2 = account[1]
+    products = product_info()
+
     id = int(input('enter an id: '))
     for i in acc['cart']:
         if id == i['order_id']:
@@ -81,9 +83,21 @@ def edit_order(user):
             if quantity < i['quantity']:
                 deducted = i['quantity'] - quantity
                 print(deducted)
-    
+                for product in products:
+                    if id == product['id']:
+                        product['stock'] = product['stock'] + deducted
+                        if id == i['order_id']:
+                            i['quantity'] = quantity
+                            i['total_price'] = quantity * i['price']
+
+                            with open(DATA_ENTRY, 'w')as file:
+                                json.dump(acc2, file, indent=4)
+                            with open(PRODUCTS_INFO, 'w')as file:
+                                json.dump(products, file, indent=4)
+                            print('product deducted successfully')
+                            return
+
     if increment:
-        products = product_info()
         for product in products:
             if id == product['id']:
                 current_q = product['stock'] - increment_count
@@ -99,7 +113,7 @@ def edit_order(user):
                     with open(PRODUCTS_INFO, 'w')as file:
                         json.dump(products, file, indent=4)
                     print('updated successfully')
-                    
+
                 if current_q < 0:
                     print('out of stock')
                     return
